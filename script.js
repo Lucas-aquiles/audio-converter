5a3227e14e4fcaa5332985b6a1d6f34ab78be2d5
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-08-17 01:34:56 -0300

    new functions

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
-class Queue {
-  constructor() {
-    this.items = [];
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const one = document.getElementById("one");
+  const two = document.getElementById("two");
+  const three = document.getElementById("three");
+  const btn = document.querySelector("#btn");
+  const btn1 = document.querySelector("#btn1");
 
-  addqueue(item) {
-    this.items.push(item);
-  }
+  class Queue {
+    constructor() {
+      this.items = [];
+    }
 
-  async subtractqueue() {
-    if (!this.isEmpty()) {
-      const item = this.items.shift();
-      await item();
+    addqueue(item) {
+      this.items.push(item);
+    }
+
+    async subtractqueue() {
+      if (!this.isEmpty()) {
+        const item = this.items.shift();
+        await item();
+      }
+    }
+
+    isEmpty() {
+      return this.items.length === 0;
+    }
+
+    async processAll() {
+      while (!this.isEmpty()) {
+        await this.subtractqueue();
+      }
     }
   }
 
-  isEmpty() {
-    return this.items.length === 0;
-  }
+  const queue = new Queue();
 
-  async processAll() {
-    while (!this.isEmpty()) {
-      await this.subtractqueue();
+  btn.addEventListener("click", () => {
+    let colorstyle = ["change", "changetwo", "changethree"];
+    let divContainer = [one, two, three];
+    for (let i = 0; i < colorstyle.length; i++) {
+      queue.addqueue(async () => {
+        console.log(` Proceso empezando ${colorstyle[i]}`);
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            divContainer[i].classList.add(colorstyle[i]);
+            resolve();
+          }, 1500)
+        );
+        console.log(` Proceso  terminando ${colorstyle[i]}`);
+      });
     }
+
+    btn.classList.add("hidden");
+    runQueue();
+  });
+
+  async function runQueue() {
+    await queue.processAll();
+    console.log("Todos los procesos han sido completados.");
   }
-}
-
-const queue = new Queue();
-
-queue.addqueue(async () => {
-  console.log("Proceso 1 empezando...");
-  await new Promise(resolve => setTimeout(resolve, 1000)); // Simulando un proceso que toma 3 segundos
-  console.log("Proceso 1 completado.");
+  btn1.addEventListener("click", () => {
+    location.reload();
+  });
 });
-
-
-
-async function runQueue() {
-  await queue.processAll();
-  console.log("Todos los procesos han sido completados.");
-}
-
-runQueue();
