137d409404a85b65b05137b37981e5f9730958e4
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-08-16 23:49:37 -0300

    add div

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ class Queue {
   }
 }
 
-// Ejemplo de uso
 const queue = new Queue();
 
 queue.addqueue(async () => {
@@ -34,17 +33,7 @@ queue.addqueue(async () => {
   console.log("Proceso 1 completado.");
 });
 
-queue.addqueue(async () => {
-  console.log("Proceso 2 empezando...");
-  await new Promise(resolve => setTimeout(resolve, 1000)); // Simulando un proceso que toma 2 segundos
-  console.log("Proceso 2 completado.");
-});
 
-queue.addqueue(async () => {
-  console.log("Proceso 3 empezando...");
-  await new Promise(resolve => setTimeout(resolve, 1000)); // Simulando un proceso que toma 4 segundos
-  console.log("Proceso 3 completado.");
-});
 
 async function runQueue() {
   await queue.processAll();
